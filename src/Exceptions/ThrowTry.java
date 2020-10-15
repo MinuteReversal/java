5fe24883bfb3c04db8b2fa312c4985986a94e9fe
MinuteReversal <114233763@qq.com>
2020-10-15 21:49:52 +0800

    ThrowTry

--- src/Exceptions/ThrowTry.java
+++ src/Exceptions/ThrowTry.java
@@ -2,6 +2,12 @@ package Exceptions;
 
 public class ThrowTry {
     public static void main(String[] args) {
-        
+        try {
+            throw new Error("hello world");
+        } catch (Error ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            System.out.println("complete");
+        }
     }
 }
