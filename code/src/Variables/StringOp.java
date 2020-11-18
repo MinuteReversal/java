dfe3d652de6e62e2fbfdcfff4add71802d9fae4e
MinuteReversal <114233763@qq.com>
2020-11-18 16:46:56 +0800

    move to code

---
+++ code/src/Variables/StringOp.java
@@ -0,0 +1,9 @@
+package Variables;
+
+public class StringOp {
+    public static void main(String[] args) {
+        String s1 = "hello world";
+        String result = s1.substring(6, 11);
+        System.out.println(result);
+    }
+}
